9974ebdae6369e3cc1e7d6ded65f13091c486ba4
sorfeb <soros21febriano@gmail.com>
2024-04-16 09:32:06 +0700

    Initialize Project Structure

---
+++ src/main/java/adpro/b10/epicarcade_auth/EpicarcadeAuthApplication.java
@@ -0,0 +1,13 @@
+package adpro.b10.epicarcade_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicarcadeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicarcadeAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adpro/b10/epicarcade_auth/EpicarcadeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package adpro.b10.epicarcade_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicarcadeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
