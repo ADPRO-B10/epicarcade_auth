59bbe7c659ad43be7d5566cacaf04fccaa68352f
codaaa19 <tsabitiphone@gmail.com>
2024-04-17 16:03:55 +0700

    test2

--- src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
+++ src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
@@ -4,18 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.ui.Model;
 
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+
     @GetMapping("")
     public String authPage(Model model){
         return "HelloWorld";
     }
+
+    
 }
 
