c9ee0f3cc6467e3006eed0342ace8002eec68711
codaaa19 <tsabitiphone@gmail.com>
2024-04-17 15:41:17 +0700

    Try to attach html

--- src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
+++ src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
@@ -2,8 +2,16 @@ package adpro.b10.epicarcade_auth.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @GetMapping("/hello")
+    public ModelAndView helloWorld() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("HelloWorld.html");
+        return modelAndView;
+    }
 }
