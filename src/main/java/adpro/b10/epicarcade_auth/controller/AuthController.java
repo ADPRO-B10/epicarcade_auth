d43c596ca8fd42c724d36142b31e4d435f54d6e2
sorfeb <soros21febriano@gmail.com>
2024-04-17 16:54:27 +0700

    Test HelloWorld.html to deployment

--- src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
+++ src/main/java/adpro/b10/epicarcade_auth/controller/AuthController.java
@@ -1,13 +1,15 @@
 package adpro.b10.epicarcade_auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
-
-
-@RestController
-@RequestMapping("/auth")
+@Controller
+@RequestMapping("/")
 public class AuthController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "HelloWorld";
+    }
 }
 
